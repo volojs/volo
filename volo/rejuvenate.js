a215d1f6b4ce5219f0989205f65e0653b8d8dada
jrburke <jrburke@gmail.com>
2012-01-01 15:59:47 -0800

    starting rejuvenate command

---
+++ volo/rejuvenate.js
@@ -0,0 +1,50 @@
+/**
+ * @license Copyright (c) 2011, The Dojo Foundation All Rights Reserved.
+ * Available via the MIT or new BSD license.
+ * see: http://github.com/jrburke/volo for details
+ */
+
+'use strict';
+/*jslint */
+/*global define, console, process */
+
+define(function (require, exports, module) {
+    var q = require('q'),
+        path = require('path'),
+        add = require('add'),
+        rejuvenate;
+
+    rejuvenate = {
+        doc: 'Updates volo.js to latest version.',
+        validate: function (namedArgs) {},
+        run: function (deferred, namedArgs, from) {
+            //Create a 'volo' directory as a sibling to the volo.js file
+            var execName = process.argv[1],
+                dirName = path.dirname(execName),
+                baseName = path.basename(execName, '.js'),
+                cwd = process.cwd(),
+                d = q.defer();
+
+            from = from || 'jrburke/volo#dist/volo.js';
+
+            //Change directory to the one holding volo.js
+            process.chdir(dirName);
+
+            function finish(result) {
+                process.chdir(cwd);
+            }
+
+            add.run(d, namedArgs, from, baseName);
+
+            q.when(d.promise, function (result) {
+                finish();
+                deferred.resolve(result + '\n' + baseName + '.js has been updated!');
+            }, function (err) {
+                finish();
+                deferred.reject(err);
+            });
+        }
+    };
+
+    return require('volo/commands').register(module.id, rejuvenate);
+});
